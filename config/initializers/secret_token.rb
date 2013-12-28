04a61fc34a082b6a9e5feec1ae9cf12e229b127e
Biju Vidyadharan <biju.vidyadharan@diaspark.com>
2013-12-28 19:14:34 +0530

    poc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SchoolingPoc::Application.config.secret_key_base = '68aad775686f2bba14395737f0a186b45052b322434b550279cddba5fd2c7b96034e64cc295eb24ecec888567ce51a0fe35ff35a519b0c7924bbebfe56b2766d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SchoolingPoc::Application.config.session_store :cookie_store, key: '_schooling_poc_session'
